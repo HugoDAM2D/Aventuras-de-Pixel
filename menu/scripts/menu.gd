7c53c0eefd1a7448189c71f13143bae39360d446
HugoDAM2D <sanchezortigas23@iessantiagohernandez.com>
2025-03-31 20:14:55 +0200

    Initial commit

---
+++ menu/scripts/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _on_jugar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_opciones_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/opciones.tscn")
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
+
+func _on_inicio_sesion_pressed() -> void:
+	pass # Replace with function body.
